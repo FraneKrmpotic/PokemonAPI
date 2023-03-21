3abb30784c45f3d9e26131324993b6da1549d993
Frane Krmpotić <franekrmpotic@gmail.com>
2023-03-21 18:41:37 +0100

    Update
    Update

--- PokemonReviewApp/Interfaces/ICategoryRepository.cs
+++ PokemonReviewApp/Interfaces/ICategoryRepository.cs
@@ -7,10 +7,6 @@ namespace PokemonReviewApp.Interfaces
         ICollection<Category> GetCategories();
         Category GetCategory(int id);
         ICollection<Pokemon> GetPokemonByCategory(int categoryId);
-<<<<<<< HEAD
         bool CategoryExists(int categoryId);
-=======
-        bool CategoryExists(int id);
->>>>>>> dbabb771d712ee8312727e5f58231582232f9871
     }
 }
--- PokemonReviewApp/Repository/CategoryRepository.cs
+++ PokemonReviewApp/Repository/CategoryRepository.cs
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
 ﻿using AutoMapper;
 using PokemonReviewApp.Data;
 using PokemonReviewApp.Interfaces;
-=======
-﻿using PokemonReviewApp.Interfaces;
->>>>>>> dbabb771d712ee8312727e5f58231582232f9871
 using PokemonReviewApp.Models;
 
 namespace PokemonReviewApp.Repository
 {
     public class CategoryRepository : ICategoryRepository
     {
-<<<<<<< HEAD
         private DataContext _context;
         private readonly IMapper _mapper;
 
@@ -23,38 +18,21 @@ namespace PokemonReviewApp.Repository
         public bool CategoryExists(int categoryId)
         {
             return _context.Categories.Any(c => c.Id == categoryId);
-=======
-        public bool CategoryExists(int id)
-        {
-            throw new NotImplementedException();
->>>>>>> dbabb771d712ee8312727e5f58231582232f9871
         }
 
         public ICollection<Category> GetCategories()
         {
-<<<<<<< HEAD
             return _context.Categories.ToList();
-=======
-            throw new NotImplementedException();
->>>>>>> dbabb771d712ee8312727e5f58231582232f9871
         }
 
         public Category GetCategory(int id)
         {
-<<<<<<< HEAD
             return _context.Categories.Where(e => e.Id == id).FirstOrDefault();
-=======
-            throw new NotImplementedException();
->>>>>>> dbabb771d712ee8312727e5f58231582232f9871
         }
 
         public ICollection<Pokemon> GetPokemonByCategory(int categoryId)
         {
-<<<<<<< HEAD
             return _context.PokemonCategories.Where(e => e.CategoryId == categoryId).Select(c => c.Pokemon).ToList();
-=======
-            throw new NotImplementedException();
->>>>>>> dbabb771d712ee8312727e5f58231582232f9871
         }
     }
 }
